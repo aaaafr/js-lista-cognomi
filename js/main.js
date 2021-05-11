9981e374512fa619b4109e20bd590cc5af5a426f
aaaafr <anastasia.frassini@icloud.com>
2021-05-11 21:13:52 +0200

    complete

--- js/main.js
+++ js/main.js
@@ -8,3 +8,12 @@ console.log(cognomi);
 var listaOrdinata = cognomi.slice();
 listaOrdinata.sort();
 console.log(listaOrdinata);
+
+// for(var i = 0; i < listaOrdinata.length; i++){
+//     var cognome = listaOrdinata[i];
+//     console.log(cognome);
+
+// }
+
+var posizione = listaOrdinata.indexOf(utenteCognome) + 1;
+console.log(posizione);
